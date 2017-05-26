025b2ea6a0c7435734f6b58ad758f88030667cc5
William Barshop <William Barshop>
2017-05-26 05:12:01 -0700

    Fixing...

--- mount_smb.py
+++ mount_smb.py
@@ -3,17 +3,18 @@ import socket
 import time
 
 #We'll wait 30 sec to make sure everything is up and running...
-time.sleep(30)
+#time.sleep(30)
 smb_ip=socket.gethostbyname("milkyway-smb")
 
-corrected_smb_ip=smb_ip.rsplit(".",1)[0]+str(int(smb_ip.rsplit(".",1)[1])+1)
-
+corrected_smb_ip=smb_ip.rsplit(".",1)[0]+"."+str(int(smb_ip.rsplit(".",1)[1])+1)
+print corrected_smb_ip
 shutil.rmtree("C:\\pulsar\\files\\staging")
 try:
-    subprocess.call(r'net use \\\\{0}\\allusers\\'.format(corrected_smb_ip))
-    subprocess.call(r'mklink /d '+'\\\\{0}\\allusers\\ '.format(corrected_smb_ip)+'C:\\pulsar\\files\\staging')
+    subprocess.call('net use \\\\{0}\\allusers /user:test test'.format(corrected_smb_ip))
+    #subprocess.call('mklink /d '+'\\\\{0}\\allusers '.format(corrected_smb_ip)+'C:\\pulsar\\files\\staging')
+    subprocess.call('mklink /d '+'C:\\pulsar\\files\\staging '+'\\\\{0}\\allusers '.format(corrected_smb_ip))
 except:
     #maybe this host has patched the problem?
-    subprocess.call(r'net use \\\\{0}\\allusers\\'.format(smb_ip))
-    subprocess.call(r'mklink /d '+'\\\\{0}\\allusers\\ '.format(smb_ip)+'C:\\pulsar\\files\\staging')
-	
+    subprocess.call('net use \\\\{0}\\allusers'.format(smb_ip))
+    subprocess.call('mklink /d '+'C:\\pulsar\\files\\staging '+'\\\\{0}\\allusers '.format(smb_ip))
+    #subprocess.call('mklink /d '+'\\\\{0}\\allusers '.format(smb_ip)+'C:\\pulsar\\files\\staging')
