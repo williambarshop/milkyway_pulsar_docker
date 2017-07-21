6d75d59d1ba638ba62a54a520fedf958789c6a97
William Barshop <wbarshop@ucla.edu>
2017-07-21 11:38:04 -0700

    Back to smb....

---
+++ mount_smb.py
@@ -0,0 +1,20 @@
+import os, subprocess
+import socket
+import time
+
+#We'll wait 30 sec to make sure everything is up and running...
+time.sleep(30)
+smb_ip=socket.gethostbyname("milkyway-smb")
+
+corrected_smb_ip=smb_ip.rsplit(".",1)[0]+"."+str(int(smb_ip.rsplit(".",1)[1])+1)
+#print corrected_smb_ip
+#os.rmdir("C:\\pulsar\\files\\staging")
+try:
+    subprocess.call('net use \\\\{0}\\allusers /user:test test'.format(corrected_smb_ip))
+    time.sleep(30)
+    subprocess.call('mklink /d '+'C:\\pulsar\\files\\staging '+'\\\\{0}\\allusers '.format(corrected_smb_ip),shell=True)
+except:
+    #maybe this host has patched the problem?
+    subprocess.call('net use \\\\{0}\\allusers /user:test test'.format(smb_ip))
+    time.sleep(30)
+    subprocess.call('mklink /d '+'C:\\pulsar\\files\\staging '+'\\\\{0}\\allusers '.format(smb_ip),shell=True)
